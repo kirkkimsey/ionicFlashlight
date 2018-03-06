f3bd3b8bae9413f83e2543ba6cd76ff479fe9e8a
Kirk Kimsey <kirkkimsey@gmail.com>
2018-03-06 10:13:29 -0500

    initial application push

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { NavController } from "ionic-angular";
+import { Flashlight } from "@ionic-native/flashlight";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
+  color = "secondary";
+  constructor(public navCtrl: NavController, private flashlight: Flashlight) {}
+  flashlightToggle() {
+    this.flashlight.toggle();
 
-  constructor(public navCtrl: NavController) {
-
+    if (this.flashlight.isSwitchedOn()) {
+      this.color = "danger";
+    } else {
+      this.color = "secondary";
+    }
   }
-
 }
